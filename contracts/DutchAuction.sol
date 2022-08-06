efffafa964606361e745f1b0c37734ad9e4bb0ed
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-08-06 21:57:52 +0300

    777

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import 'hardhat/console.sol';
-
 contract DutchAuction {
     uint private constant DURATION = 2 days;
     address payable public immutable seller;
@@ -53,4 +51,4 @@ contract DutchAuction {
         stopped = true;
         emit Bought(price, msg.sender);
     }
-} 
+}
