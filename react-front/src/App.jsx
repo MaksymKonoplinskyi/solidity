68606df81ee1697c9b2e5e452c975a992c88204d
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-08-06 17:48:46 +0300

    3

--- react-front/src/App.jsx
+++ react-front/src/App.jsx
@@ -67,19 +67,19 @@ export class App extends Component {
       this._provider.getSigner(0)
     )
 
+       this.setState({
+      selectedAccount: selectedAddress
+    }, async () => {
+      await this.updateBalance()
+    })
+
     if (await this.updateStopped()) { return }
 
     this.startingPrice = await this._auction.startingPrice()
     this.startAt = ethers.BigNumber.from(await this._auction.startAt() * 1000)
     this.discountRate = await this._auction.discountRate()
 
-    this.setState({
-      selectedAccount: selectedAddress
-    }, async () => {
-      await this.updateBalance()
-    })
-
-    this.checkPriceInterval = setInterval(() => {
+     this.checkPriceInterval = setInterval(() => {
       const eclapsed = ethers.BigNumber.from(
         Date.now()
       ).sub(this.startAt)
